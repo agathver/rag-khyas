7b03da762c9772455a64da6d2476abee959bca9f
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2018-11-12 23:25:10 +0530

    Add bot GPIO control

---
+++ control_server/bot.py
@@ -0,0 +1,127 @@
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
+from Queue import Queue
+from threading import Thread
+
+queue = Queue()
+
+loop = True
+
+def setup():
+    GPIO.setup("P8_7", GPIO.OUT)
+    GPIO.setup("P8_8", GPIO.OUT)
+    GPIO.setup("P8_9", GPIO.OUT)
+    GPIO.setup("P8_10", GPIO.OUT)
+    GPIO.setup("P8_11", GPIO.OUT)
+    GPIO.setup("P8_12", GPIO.OUT)
+    GPIO.setup("P8_14", GPIO.OUT)
+    GPIO.setup("P8_15", GPIO.OUT)
+    GPIO.setup("P8_16", GPIO.OUT)
+    GPIO.setup("P8_17", GPIO.OUT)
+    GPIO.setup("P8_18", GPIO.OUT)
+    GPIO.setup("P8_26", GPIO.OUT)
+    GPIO.setup("P9_23", GPIO.OUT)
+    GPIO.setup("P9_25", GPIO.OUT)
+    GPIO.setup("P9_27", GPIO.OUT)
+    GPIO.setup("P9_15", GPIO.OUT)
+
+    servo_pin = "P8_13"
+    duty_min = 3
+    duty_max = 14.5
+    duty_span = duty_max - duty_min
+
+    PWM.start(servo_pin, (100-duty_min), 60.0)
+    duty = 100 - ((30 / 180) * duty_span + duty_min)
+
+def start():
+    global loop
+
+    loop = True
+    t = Thread(target=loop)
+    t.daemon = True
+    t.start()
+
+def dispatch(action):
+    global queue
+
+    queue.put(action)
+
+def loop():
+    global loop
+
+    while loop:
+        action = queue.get()
+
+        if action == 'bot_left':
+            bot_left()
+        elif action == 'bot_right':
+            bot_right()
+        elif action == 'bot_forward':
+            bot_forward()
+        elif action == 'bot_stop':
+            bot_stop()
+        elif action == 'plat_left':
+            plat_left()
+        elif action == 'plat_right':
+            plat_right()
+        elif action == 'servo_up':
+            servo_up()
+        elif action == 'servo_down':
+            servo_down()
+
+
+def bot_left():
+    GPIO.output("P8_8", GPIO.LOW)
+    GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output("P8_12", GPIO.HIGH)
+    GPIO.output("P8_14", GPIO.LOW)
+    time.sleep(2)
+
+def bot_right():
+    GPIO.output("P8_8", GPIO.HIGH)
+    GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output("P8_12", GPIO.LOW)
+    GPIO.output("P8_14", GPIO.LOW)
+    time.sleep(2)
+
+def bot_forward():
+    GPIO.output("P8_8", GPIO.HIGH)
+    GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output("P8_12", GPIO.HIGH)
+    GPIO.output("P8_14", GPIO.LOW)
+    time.sleep(2)
+
+def bot_stop():
+    GPIO.output("P8_8", GPIO.LOW)
+    GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output("P8_12", GPIO.LOW)
+    GPIO.output("P8_14", GPIO.LOW)
+    time.sleep(2)
+
+def plat_left():
+    GPIO.output("P8_15", GPIO.HIGH)
+    GPIO.output("P8_17", GPIO.LOW)
+    time.sleep(2)
+
+def plat_right():
+    GPIO.output("P8_15", GPIO.LOW)
+    GPIO.output("P8_17", GPIO.HIGH)
+    time.sleep(2)
+
+def servo_up():
+    GPIO.output("P8_9", GPIO.HIGH)
+    GPIO.output("P8_11", GPIO.LOW)
+    PWM.set_duty_cycle(servo_pin, duty)
+    PWM.stop(servo_pin)
+    PWM.cleanup()
+
+def servo_down():
+    GPIO.output("P8_9", GPIO.LOW)
+    GPIO.output("P8_11", GPIO.HIGH)
+    PWM.set_duty_cycle(servo_pin, duty)
+    PWM.stop(servo_pin)
+    PWM.cleanup()
+
+def stop():
+    global loop
+    loop = False
--- control_server/server.py
+++ control_server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
+import bot
 
 app = Flask(__name__)
 
@@ -15,31 +16,37 @@ socketio = SocketIO(app)
 @socketio.on('move forward')
 def move_forward():
     print('forward', 0.1)
+    bot.bot_forward()
 
 
 @socketio.on('move backward')
 def move_backward():
     print('backward', 0.1)
+    bot.bot_stop()
 
 
 @socketio.on('turn left')
 def turn_left():
     print('left', 0.1)
+    bot.bot_left()
 
 
 @socketio.on('turn right')
 def turn_right():
     print('right', 0.1)
+    bot.bot_right()
 
 
 @socketio.on('rotate arm left')
 def arm_rotate_left():
     print('arm rotate left', 0.1)
+    bot.plat_left()
 
 
 @socketio.on('rotate arm right')
 def arm_rotate_right():
     print('arm rotate right', 0.1)
+    bot.plat_right()
 
 
 # if __name__ == '__main__':
